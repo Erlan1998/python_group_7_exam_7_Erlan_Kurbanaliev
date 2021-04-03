2219ed061e9b412b449069ce7bdbc48e708e1906
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-04-03 12:56:38 +0600

    Реализовал Модельку и Админку для вопросов и ответов!

--- questionnaire/webapp/admin.py
+++ questionnaire/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from webapp.models import Poll, Choice
 # Register your models here.
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_date']
+    list_filter = ['created_date']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- questionnaire/webapp/models.py
+++ questionnaire/webapp/models.py
@@ -11,7 +11,29 @@
 # Зарегистрируйте модели в админ-панели и добавьте тестовые данные, создайте фикстуру.
 
 
-# from django.db import models
+from django.db import models
 # from webapp.validators import MinLengthValidator, CapitalLetter, OnlyLetters
-#
-# class Poll(models.Model):
+
+
+class Poll(models.Model):
+    question = models.TextField(max_length=200, null=False, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Голос'
+        verbose_name_plural = 'Голосы'
+
+    def __str__(self):
+        return f'{self.question}'
+
+class Choice(models.Model):
+    answer = models.TextField(max_length=200, null=False, blank=False)
+    poll = models.ForeignKey('webapp.Poll', related_name='Choices', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Choices'
+        verbose_name = 'Выбор'
+        verbose_name_plural = 'Выборы'
+
+    def __str__(self):
+        return f'{self.id}. {self.answer}: {self.poll}'
